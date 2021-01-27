57ededf5e3158ddfac8d047d0fc9dc272bf35f6c
Stephen Osentoski <stephenosentoski@Stephens-MacBook-Pro.local>
2021-01-27 08:54:25 -0500

    Finished reader, now exporting clips

--- file-reader.py
+++ file-reader.py
@@ -1,11 +1,20 @@
 import time
 import re
 import os
-from cv2 import cv2
+import shutil
+import cv2
+import math
+import numpy as np
+import PIL
+from PIL import Image 
+import pytesseract
+import moviepy
+from pytesseract import *
+from moviepy.editor import *
 
 def read_file(txt_file):
     times = []
-    expression = '^(0?[1-9]|1[0-9]):[0-5][0-9]'
+    expression = '(0?[1-9]|1[0-9]):[0-5][0-9]'
     pattern = re.compile(expression)
 
     with open(txt_file, 'r') as f:
@@ -16,30 +25,102 @@ def read_file(txt_file):
 
     return times
 
-def create_clips(timestamps, video):
-
-    if not os.path.exists('img_frames'):
+def create_frame_dir():
+    if not os.path.exists('image_frames'):
+        os.makedirs('image_frames')
+    else:
+        shutil.rmtree('image_frames')
         os.makedirs('image_frames')
 
-    test_vid = cv2.VideoCapture(video)
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = Image.open(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
 
-    prev_time = timestamps[0]
+def get_game_time(pattern, image):
+    text = pytesseract.image_to_string(image,lang='eng')
+    text = ' '.join(text.split())
+    print(text)
+    game_time = re.search(pattern, text)
+    if (game_time):
+        return game_time.group()
+    else:
+        return None
+
+def get_sec(time_str):
+    m, s = time_str.split(':')
+    return int(m) * 60 + int(s)
+
+def fast_forward(vidcap, time_diff, fps):
+    curr_frame = int(vidcap.get(cv2.CAP_PROP_POS_FRAMES))
+    new_frame = curr_frame + (time_diff * fps)
+    print ("Curr_frame = ", curr_frame)
+    print ("time_diff = ", time_diff)
+    print ("new_frame = ", new_frame)
+    vidcap.set(cv2.CAP_PROP_POS_FRAMES, new_frame)
+    print('New Position:', int(vidcap.get(cv2.CAP_PROP_POS_FRAMES)))
+
+def cut_clip(vidcap, game_time):
+    print("Cuting the clip")
+    clip = VideoFileClip("G14-Purdue-5min.mp4")
+
+    vidcap_pos = (vidcap.get(cv2.CAP_PROP_POS_MSEC) / 1000)
+    start_ts = vidcap_pos - 7
+    end_ts = vidcap_pos + 3
+
+    clip = clip.subclip(start_ts, end_ts)
+    filename = 'exp_' + game_time + '.mp4'
+    clip.write_videofile(filename)
+
+def create_clips(timestamps, videoFile):
+
+    create_frame_dir()
+
+    expression = '(0?[1-9]|1[0-9]):[0-5][0-9]'
+    pattern = re.compile(expression)
+
+    vidcap = cv2.VideoCapture(videoFile)
+    success,image = vidcap.read()
+    target_time = get_sec(timestamps.pop(0))
     time_diff = 0
 
-    for time in timestamps:
-        #Grab screenshot from video
+    #Set to 5 seconds until we find a game time
+    seconds = 5
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    multiplier = fps * seconds
 
-        #Convert text from screenshot
+    while success:
+        frameId = int(round(vidcap.get(1)))
+        success, image = vidcap.read()
 
-        #See if screenshot contains 'time'
+        if frameId % multiplier == 0:
+            game_time = get_game_time(pattern, image)
+            
+            if (game_time):
+                print ("Found game time: ", game_time)
+                game_time_sec = get_sec(game_time)
+                
+                if (game_time_sec == target_time):
+                    print("Found time we want!", game_time)
+                    #TODO - Cut video
+                    cut_clip(vidcap, game_time)
+                    target_time = get_sec(timestamps.pop(0))
+                else:
+                    multiplier = fps * 0.5 #We now have a game time, 
+                
+                time_diff = game_time_sec - target_time
+                fast_forward(vidcap, time_diff, fps)
 
-
-        print(time)
+    vidcap.release()
+    print("Complete")
 
 def main():
     # Define the files to work with
     filename = 'timestamps.txt'
-    video = 'testvid.mp4'
+    video = 'G14-Purdue-5min.mp4'
 
     # Read the tiemstamps into a list
     timestamps = read_file(filename)
